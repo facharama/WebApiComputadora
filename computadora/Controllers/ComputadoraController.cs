63ec6bf49f3f6a51d6d2d4af6a53e1de7397aac2
rama <ramirogbazan@gmail.com>
2026-08-19 22:53:28 -0300

    Agregar archivos de proyecto.

---
+++ computadora/Controllers/ComputadoraController.cs
@@ -0,0 +1,66 @@
+﻿using computadora.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace computadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputadoraController : ControllerBase
+    {
+        // GET: api/Computadora
+        [HttpGet]
+        public Computadora Get()
+        {
+            Computadora compu = new Computadora();
+
+            compu.marca = "Apple";
+            compu.procesador = "M5 Pro";
+            compu.ram = 16;
+
+            List<string> programas = new List<string>();
+
+            programas.Add("Safari");
+            programas.Add("Chrome");
+            programas.Add("Visual Studio");
+
+            compu.programasInstalados = programas;
+
+            return compu;
+        }
+
+
+        // GET: api/Computadora/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id == 0)
+            {
+                return "ERROR";
+            }
+
+            return "OK";
+        }
+
+
+        // POST: api/Computadora
+        [HttpPost]
+        public string Post(Computadora compu)
+        {
+            return compu.marca;
+        }
+
+
+        // PUT: api/Computadora/5
+        [HttpPut("{id}")]
+        public void Put(int id, Computadora compu)
+        {
+        }
+
+
+        // DELETE: api/Computadora/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ computadora/Models/Computadora.cs
@@ -0,0 +1,13 @@
+﻿namespace computadora.Models
+{
+    public class Computadora
+    {
+        public string procesador { get; set; }
+
+        public int ram { get; set; }
+
+        public string marca { get; set; }
+
+        public List<string> programasInstalados { get; set; }
+    }
+}
